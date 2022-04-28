cdc565685fc2748641699450dfe7bdeed94de553
Abdullah Said <abdullps401@gmail.com>
2022-04-28 22:36:27 +0700

    fix success template data length displaying 0

--- template.py
+++ template.py
@@ -3,7 +3,7 @@ from flask import make_response, jsonify
 def success(values):
     res = {
         "status" : 200,
-        "length" : 0,
+        "length" : len(values),
         "data" : values
     }
     return make_response(jsonify(res), 200)
